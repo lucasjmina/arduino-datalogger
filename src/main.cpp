d6965ca9076fc17372173e54febf60c4ae95c951
Lucas <lucas_git.nvgw4@slmail.me>
2023-12-18 15:19:02 -0300

    refactor: Translation to English
    Decided to use English from now on, everything will be translated.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 /*
  * main.cpp
- * Autor/es: Lucas J. Mina. Dario D. Larrea & Miriam P. Damborsky
- * Descripción: Código para prototipo de datalogger de humedad y temperatura usando sensor DHT y 
- * Arduino Nano. Nombre clave "Oreo".
- *
+ * Author/s: Lucas J. Mina. Dario D. Larrea & Miriam P. Damborsky
+ * Description: Code for a humidity and temperatura detalogger prototipe using a DHT sensor and
+ * Arduino. Codename "Oreo".
+ * 
  * TODO:
- * - Mejorar sleep para mayor ahorro de energía https://www.gammon.com.au/power
- * - Límite de mediciones usando conteo o una fecha/hora (usando alarma 2 del RTC?)
- * - Agregar alguna forma para chequear que los sensores y módulos estén conectados/funcionen
- * correctamente
+ * - Sleep "better" for more energy saving https://www.gammon.com.au/power
+ * - Set a limit for measurement using a counter or date/time (RTC's alarm 2 maybe?)
+ * - Find a way to check that sensors and other modules are connected and working correctly.
  */
 
 #include <Arduino.h>
@@ -20,10 +19,10 @@
 #include <SPI.h>
 #include <avr/sleep.h>
 
-//1 para sincronizar hora del RTC con el sistema
+//1 for sincronizing RTC time with system's
 #define set_time 0
 
-//Pines LCD
+//LCD pins definition
 #define rs 10
 #define rw 9
 #define en 8
@@ -38,7 +37,7 @@
 #define bclk_pin 16
 #define button_pin 3
 
-//Intervalo para lectura de datos (días, horas, minutos, segundos)
+//Measurement taking interval (days, hours, minutes, seconds)
 TimeSpan interval = TimeSpan(0, 0, 1, 0);
 int count = 1;
 DateTime time_now;
@@ -88,30 +87,29 @@ void setup() {
         while(1);
     }
 
-    //Generacion de nombre único usando timestamp
+    //Unique name for new files using timestamp (YYYYMMDDhhmmss_DATALOG.csv)
     time_now = rtc.now();
     time_now.toString(filename);
 
-    //Header para el csv
+    //CSV header
     File datalog = SD.open(filename, FILE_WRITE);
-    datalog.println("numero,fecha,hora,temperatura,humedad");
+    datalog.println("number,date,time,temperature,humidity");
     datalog.close();
 
-    //En "OFF", pin sqw está normalmente alto y baja cuando la alarma "suena"
+    //When "off" SQW pin is normally high and goes low when alarm "rings"
     rtc.writeSqwPinMode(DS3231_OFF);
-    //Asegurando que no hay alarmas
+    //Making sure no alarms are set
     rtc.disableAlarm(1);
     rtc.disableAlarm(2);
     rtc.clearAlarm(1);
     rtc.clearAlarm(2);
-    //Intervalo para la primer medición, se puede usar uno más corto, por ejemplo
-    //un minuto: rtc.SetAlarm1(time_now + TimeSpan(0, 0, 1, 0), DS3231_A1_Second)
-    //Las siguientes mediciones continuan con el intervalo elegido en "interval"
+    //Time interval for first measurement after powering on. Following measurements
+    //use the time from variable "interval"
     rtc.setAlarm1(rtc.now() + TimeSpan(0, 0, 0, 3), DS3231_A1_Second);
     
     pinMode(int_pin, INPUT_PULLUP);
     pinMode(button_pin, INPUT_PULLUP);
-    //Modo FALLING para evitar que la interrupción se active al desacativar la alarma
+    //FALLING mode so interrupt doesn't trigger when turning off the alarm
     attachInterrupt(digitalPinToInterrupt(int_pin), int00_isr, FALLING);
     attachInterrupt(digitalPinToInterrupt(button_pin), int01_isr, FALLING);
     
@@ -160,7 +158,7 @@ void loop() {
     else if (millis() - previous_millis >= 3000) {
         digitalWrite(bclk_pin, LOW);
     }
-    //NO suspender si el LCD está encendido o hay mediciones pendientes
+    //Do NOT suspend MCU if LCD backlight is on or there are pending measurements
     if (digitalRead(bclk_pin) == LOW && write == false) {
         sleep_time();
     }
